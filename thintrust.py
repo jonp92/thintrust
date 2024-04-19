e9a15064d2ae18332be13322f230477e46f780d1
jonathan <jonathanpressler@icloud.com>
2024-04-19 14:50:05 -0400

    Refactor ThinTrust class to display a more informative error message when not run as root

--- thintrust.py
+++ thintrust.py
@@ -8,7 +8,7 @@ from utils.system_profiler import SystemProfiler
 class ThinTrust(Logger):
     def __init__(self):
         if os.geteuid() != 0:
-            print('Please run as root.')
+            print('ThinTrust must be run as root. Please run with sudo or as root.')
             exit(1)
         self.config = json.load(open('config.json')) if os.path.exists('config.json') else None
         if not self.config:

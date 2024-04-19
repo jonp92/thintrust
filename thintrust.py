ff2a28d69ea5c0738a8681f2da7751f1b75e784f
jonathan <jonathanpressler@icloud.com>
2024-04-19 13:47:35 -0400

    Refactor ThinTrust class to fix subprocess call in get_cpu_info method

--- thintrust.py
+++ thintrust.py
@@ -41,7 +41,7 @@ class ThinTrust(Logger):
     
     def get_cpu_info(self):
         try:
-            verbose_info = return subprocess.check_output('lscpu', shell=True).decode('utf-8').strip()
+            verbose_info = subprocess.check_output('lscpu', shell=True).decode('utf-8').strip()
             vendor = verbose_info.split('Vendor ID:')[1].split('\n')[0].strip()
             model = verbose_info.split('Model name:')[1].split('\n')[0].strip()
             cores = verbose_info.split('CPU(s):')[1].split('\n')[0].strip()

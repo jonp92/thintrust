b39bbfe9b5edecf0e56b5cef452079b384d64b9f
jonathan <jonathanpressler@icloud.com>
2024-04-19 13:59:47 -0400

    Refactor ThinTrust class to handle system memory units less than 1 GB and improve system profile logging

--- thintrust.py
+++ thintrust.py
@@ -58,15 +58,18 @@ class ThinTrust(Logger):
         try:
             memory = psutil.virtual_memory()
             human_readable = ['total', 'available', 'used', 'free']
-            # if less than 1GB, show in MB
-            if memory.total < 1024 ** 3:
+            if memory.total < 1024 ** 2:
+                # if less than 1MB, show in KB
+                human_readable_values = [round(getattr(memory, attr) / 1024, 2) for attr in human_readable]
+            elif memory.total < 1024 ** 3:
+                # if less than 1GB, show in MB
                 human_readable_values = [round(getattr(memory, attr) / (1024 ** 2), 2) for attr in human_readable]
             elif memory.total < 1024 ** 4:
                 # if less than 1TB, show in GB
                 human_readable_values = [round(getattr(memory, attr) / (1024 ** 3), 2) for attr in human_readable]
             else:
-                # if less than 1GB, don't alter
-                human_readable_values = [getattr(memory, attr) for attr in human_readable]
+                # if more than 1TB, show in TB
+                human_readable_values = [round(getattr(memory, attr) / (1024 ** 4), 2) for attr in human_readable]
             return dict(zip(human_readable, human_readable_values))
         except Exception as e:
             self.logger.error(f'Error getting system memory: {e}')

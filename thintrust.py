debb2a65f2ab1c59dde8e5c7aa9382b033b2afc0
jonathan <jonathanpressler@icloud.com>
2024-04-20 16:29:22 -0400

    Refactor package installation in ThinTrust initialization

--- thintrust.py
+++ thintrust.py
@@ -2,7 +2,7 @@ import os
 import json
 from utils.logger import Logger
 import subprocess
-
+from utils.system_profiler import SystemProfiler
 
 class ThinTrust(Logger):
     def __init__(self):
@@ -20,7 +20,6 @@ class ThinTrust(Logger):
         if not self.install_initial_packages():
             self.logger.error(f'Error installing initial packages:{self.initial_packages}\n Try installing them manually and running ThinTrust again.')
             exit(1)
-        from utils.system_profiler import SystemProfiler
         self.system_profiler = SystemProfiler(self.logger).system_profile
         
     def install_initial_packages(self):
@@ -29,6 +28,7 @@ class ThinTrust(Logger):
             subprocess.check_output(f'apt-get update -y', shell=True)
             subprocess.check_output(f'apt-get install -y {" ".join(self.initial_packages)}', shell=True)
             subprocess.check_output('python3 -m venv .venv', shell=True)
+            subprocess.check_output('pip3 install psutil --break-system-packages', shell=True)
             subprocess.check_output('.venv/bin/pip3 install -r requirements.txt', shell=True) 
             return True
         except Exception as e:

ae8303b300aade1296d6d089118b907dc8a16bf4
jonathan <jonathanpressler@icloud.com>
2024-04-19 20:04:05 -0400

    Refactor setup_overlayroot method in InitialSetup class to include debug print statement

--- setup/setup.py
+++ setup/setup.py
@@ -11,6 +11,7 @@ class InitialSetup(ThinTrust):
         script_dir = os.path.dirname(os.path.abspath(__file__))
         setup_file = f'{script_dir}/setup.json'
         super().__init__()
+        print(dir(self))
         self.logger.name = 'InitialSetup'
         self.logger.info('Starting initial setup...')
         self.sevenzip = SevenZip()

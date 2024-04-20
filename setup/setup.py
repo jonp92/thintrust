8ce215800d5f92632ee6ba05abdaec3d8431cd25
jonathan <jonathanpressler@icloud.com>
2024-04-19 20:09:59 -0400

    Refactor setup_overlayroot method in InitialSetup class to use subprocess.check_output for downloading and decompressing the splash theme

--- setup/setup.py
+++ setup/setup.py
@@ -121,8 +121,7 @@ class InitialSetup(ThinTrust):
         def change_splash(self):
             try:
                 os.makedirs('/usr/share/plymouth/themes/thintrust', exist_ok=True)
-                with open('plymouthsplash.7z', 'wb') as f:
-                    f.write(requests.get(f'https://thintrust.com/release/{self.distro_version}/plymouthsplash.7z').content)
+                subprocess.check_output(f'wget https://thintrust.com/release/{self.distro_version}/plymouthsplash.7z', shell=True)
                 subprocess.check_output(f'{self.sevenzip.path} x plymouthsplash.7z -o/usr/share/plymouth/themes/thintrust', shell=True)
                 subprocess.check_output('plymouth-set-default-theme -R thintrust', shell=True)
                 return True

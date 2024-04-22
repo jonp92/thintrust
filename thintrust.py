8b1923542a0e402605382c41cab8ddb36325c8a5
jonathan <jonathanpressler@icloud.com>
2024-04-22 11:22:01 -0400

    Refactor ThinTrust.py to include pretty version in argparse and print statement

--- thintrust.py
+++ thintrust.py
@@ -40,6 +40,7 @@ class ThinTrust(Logger):
             exit(1)
         for key, value in self.config.items():
             setattr(self, key, value)
+        self.pretty_version = f'v{self.distro_version} {self.distro_release.capitalize()}'
         super().__init__('ThinTrust', self.log_file, self.log_level)
         # if not self.install_initial_packages():
         #     self.logger.error(f'Error installing initial packages:{self.initial_packages}\n Try installing them manually and running ThinTrust again.')
@@ -74,7 +75,7 @@ if __name__ == '__main__':
     parser.epilog = 'ThinTrust is a tool for setting up and managing ThinTrust OS endpoints.\n'
     args = parser.parse_args()
     if args.version:
-        print(f'ThinTrust Release: {thintrust.distro_release} Version: {thintrust.distro_version}')
+        print(f'ThinTrust Release: {thintrust.pretty_version}')
     elif args.setup:
         thintrust.install_initial_packages()
         thintrust.initial_setup(thintrust)
